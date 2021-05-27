019968f3878dced05e8ae17eb717039ec421cc7d
pieshakelbery <karol.szum13@gmail.com>
2021-05-27 16:55:41 +0200

    Add database connection; Edit dockerfile

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -1,13 +1,13 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 require_once __DIR__.'/../models/User.php';
 
 class SecurityController extends AppController {
 
     public function login() {
-        $user = new User('test@test.com', 'admin', 'administrator', 'Kowalski');
-
+        $userRepository = new UserRepository();
 
         if(!$this->isPost()) {
             return $this->login('login');
@@ -15,6 +15,12 @@ class SecurityController extends AppController {
 
         $email = $_POST["email"];
         $password = $_POST["password"];
+
+        $user = $userRepository->getUser($email);
+
+        if(!$user) {
+            return $this->render('login', ['messages'=>'User not exist!']);
+        }
 
         if($user->getEmail() !== $email){
             return $this->render('login', ['messages'=>'User with this email not exist!']);
@@ -25,7 +31,5 @@ class SecurityController extends AppController {
 
         $url = "http://$_SERVER[HTTP_HOST]";
         header("Location: {$url}/products");
-
-        //return $this->render('products');
     }
 }
